ce5def413a4bd9fbf50b6a8b4f191970cce1e67b
Maxime Borry <borry@mpi-sdag1.sdag.ppj.shh.mpg.de>
2019-11-17 21:47:40 +0100

    update st2 prepare

--- sbatch_st2.sh
+++ sbatch_st2.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=borry@shh.mpg.de # Where to send mail	
 #SBATCH --ntasks=1                   # Run a single task
 #SBATCH --partition=medium	
-#SBATCH --cpus-per-task=6            # Number of CPU cores per task
+#SBATCH --cpus-per-task=8            # Number of CPU cores per task
 #SBATCH --mem=20gb                   # Job memory request
 #SBATCH --time=47:59:00              # Time limit hrs:min:sec
 #SBATCH --output=st2_%j.log     # Standard output and error log
@@ -14,7 +14,7 @@ pwd; hostname; date
 sourcetracker2 gibbs -i /projects1/users/borry/33_marsha_wibowo/wibowo_kostic_coprolite/all_samp_st2.biom \\
                      -m /projects1/users/borry/33_marsha_wibowo/wibowo_kostic_coprolite/labels_st2.tsv \\
                      -o wib_st2 \\
-                     --source_rarefaction_depth 2603493 \\
-                     --sink_rarefaction_depth 2603493 \\
-                     --jobs 6
+                     --source_rarefaction_depth 10470 \\
+                     --sink_rarefaction_depth 10470 \\
+                     --jobs 8
 date
